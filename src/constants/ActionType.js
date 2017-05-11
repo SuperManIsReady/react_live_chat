506e1450236f81b87a24163b00ec0538ca5ef952
Ashik <supermanisready@gmail.com>
2017-05-11 18:24:28 +0530

    Create ActionType.js

---
+++ src/constants/ActionType.js
@@ -0,0 +1,18 @@
+
+export const SET_MY_LOGIN = 'set_my_login';
+
+export const SET_MY_USERS = 'set_my_users';
+
+export const SET_MY_MESSAGE = "set_my_message";
+
+export const ADD_TO_ACTIVE_CHAT = "add_to_active_chat";
+
+export const  SET_TO_ACTIVE_CHAT = "set_to_active_chat";
+
+export const ADD_TO_LATER_CHAT = "add_to_later_chat";
+
+export const SEND_MESSAGE = "send_message";
+
+export const SET_MY_EMOJI = "set_my_emoji";
+
+export const TYPING = "typing";
